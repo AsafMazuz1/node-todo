878be1d63b7571f8b82c0d5769322da374c44725
asafmazuz <asafmazuz@hotmail.com>
2018-09-10 17:41:06 +0300

    Add GET todo by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 
 var {mongoose} = require('./db/mongoose');
@@ -28,6 +29,23 @@ app.get('/todos' , (req , res) =>{
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+
+app.get('/todos/:id' , (req , res) =>{
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send('Id not valid');
+	}
+	Todo.findById(id).then((todo) => {
+		if(!todo){
+			return res.status(404).send('Id not found');
+		}
+		res.send({todo});
+	}, (e) => {
+		res.status(400).send('Error accured');
+	});
+
 });
 
 app.listen(3000 , () => {
