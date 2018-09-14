5234e6a3ace5143672050deb10106e47db070020
asafmazuz <asafmazuz@hotmail.com>
2018-09-14 16:38:35 +0300

    Add hash to pass

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,24 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc!';
+
+/*
+bcrypt.genSalt(10 , (err, salt) =>{
+	bcrypt.hash(password , salt , (err , hash) =>{
+		console.log(hash);
+	});
+});
+*/
+var hashPassword = '$2a$10$C5ngIUnwP37MQML11sN66.z.j5NZfHRG17q5Aihw5H95cKncz9u6S';
+
+bcrypt.compare(password , hashPassword, (err , res) => {
+	console.log(res);
+});
+
+
+/*
 var data = {
 	id: 10
 };
@@ -12,7 +30,7 @@ console.log('token', token);
 var decoded = jwt.verify(token , '123abc');
 console.log('decided', decoded);
 
-/*
+
 var message = "I am user 3";
 var hash = SHA256(message).toString();
 
